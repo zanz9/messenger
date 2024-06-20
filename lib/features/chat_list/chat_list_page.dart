2167f378de8b3fcfa2f7d7f3d7727dcd3f0c987c
zAnz <baidosovich@gmail.com>
2024-06-20 09:10:04 +0500

    chat list, theme colors

---
+++ lib/features/chat_list/chat_list_page.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:messenger/theme.dart';
+
+class ChatListPage extends StatelessWidget {
+  const ChatListPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            surfaceTintColor: Colors.white,
+            forceElevated: true,
+            elevation: 2,
+            bottom: PreferredSize(
+              preferredSize: const Size(double.infinity, 16),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: CustomColors.stroke,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            const Icon(Icons.search, color: CustomColors.gray),
+                            const SizedBox(width: 8),
+                            Text(
+                              'Поиск',
+                              style: theme.textTheme.bodyLarge!.copyWith(
+                                  color: CustomColors.gray,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 4)
+                  ],
+                ),
+              ),
+            ),
+            expandedHeight: 120,
+            floating: false,
+            pinned: true,
+            snap: false,
+            flexibleSpace: FlexibleSpaceBar(
+              background: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 12),
+                  child: SizedBox(
+                    height: 80,
+                    child: Text(
+                      'Чаты',
+                      style: theme.textTheme.titleLarge!
+                          .copyWith(fontWeight: FontWeight.w600, fontSize: 36),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            sliver: SliverList.separated(
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.green,
+                          radius: 25,
+                          child: Text('ВВ', style: TextStyle(fontSize: 20)),
+                        ),
+                        const SizedBox(width: 12),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Виктор Власов',
+                              style: theme.textTheme.bodyLarge!.copyWith(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Вы: ',
+                                  style: theme.textTheme.bodyMedium,
+                                ),
+                                Text(
+                                  'Привет!',
+                                  style: theme.textTheme.bodyMedium!.copyWith(
+                                    color: CustomColors.darkGray,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Вчера',
+                      style: theme.textTheme.bodySmall!.copyWith(
+                        color: CustomColors.darkGray,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              separatorBuilder: (context, index) => const Divider(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color stroke = Color(0xFFEDF2F6);
+  static const Color gray = Color(0xFF9DB7CB);
+  static const Color darkGray = Color(0xFF5E7A90);
+  static const Color black = Color(0xFF2B333E);
+  static const Color green = Color(0xFF3CED78);
+  static const Color darkGreen = Color(0xFF00521C);
+}
